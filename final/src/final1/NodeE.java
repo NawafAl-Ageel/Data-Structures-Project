b103e7dd6f047f3ac88b5c6d825f7cdab40b1f71
M-albab10 <146444381+M-albab10@users.noreply.github.com>
2023-10-07 17:05:44 +0300

    Add files via upload

---
+++ final/src/final1/NodeE.java
@@ -0,0 +1,30 @@
+package final1;
+
+public class NodeE {
+	
+	public class Node {
+		
+		public Event data;
+		public Node next;
+		
+		
+		
+		
+		public Node() {
+			data=null;
+			next=null;
+		}
+
+
+
+
+		public Node(Event data) {
+			next=null;
+			this.data = data;
+		}
+		
+		
+
+	}
+
+}
