be872100a50f007afe5240a5adf49365b4c50853
NawafAl-Ageel <146536903+NawafAl-Ageel@users.noreply.github.com>
2023-10-09 00:12:01 +0300

    Update phonebook.java Class Phonebook Almost Done!

--- final/src/final1/phonebook.java
+++ final/src/final1/phonebook.java
@@ -3,96 +3,124 @@ package final1;
 import java.util.*;
 
 public class Phonebook {
-	public static void main(String[] args) {
-	LinkedList l = new LinkedList();
-	Scanner input = new Scanner(System.in);
-	int choice = 0;
 	
-	do {
-		System.out.println("");
-		System.out.println("Welcome to the Linked Tree Phonebook!");
-		System.out.println();
-		System.out.println("Pleas choose an option");
-		System.out.println("1- Add a Contact");
-		System.out.println("2- Search for a contact");
-		System.out.println("3- Delete a contact");
-		System.out.println("4- Schesule an event");
-		System.out.println("5- Print event details");
-		System.out.println("6- Print contact by first name");
-		System.out.println("7- Print all events alphabetically");
-		System.out.println("8- Exit");
-		choice = input.nextInt();
+	LinkedList contactsList = new LinkedList();
+	LinkedList eventsList = new LinkedList();
+	
+	public void addContact(String name, String phoneNumber, String emailAddress, String address,String birthday, String notes) {
+		Contact contact = new Contact(name, phoneNumber, emailAddress, address, birthday, notes);
+		contactsList.addContact(contact);
+	}
+	
+	
+	
+	public void searchContact() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter search criteria: ");
+		System.out.println("1. Name");
+		System.out.println("2. Phone Number");
+		System.out.println("3. Email Address");
+		System.out.println("4. Address");
+		System.out.println("5. Birthday");
+		int choice = input.nextInt();
+		input.nextLine(); // to finish the line
+
+		String searchedCriteria = null;
+		switch(choice) {
+		case 1: System.out.println("Enter the Contact's name: ");  searchedCriteria = input.next();
+		case 2: System.out.println("Enter Phone Number: ");		 searchedCriteria = input.next();
+		case 3:	System.out.println("Enter Email Address: ");searchedCriteria = input.next();
+		case 4:	System.out.println("Enter Address: ");		 searchedCriteria = input.next();
+		case 5:	System.out.println("Enter Birthday:");	 searchedCriteria = input.next();			
+			default:
+				System.out.println("Wrong Number choosed!");
+		}
+		Contact searchedContact = contactsList.searchByAnyTerm(searchedCriteria);
+		contactsList.printContactInfo(searchedContact);
+	}
+	
+	public void deleteContact() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the contact's name that you want to delete: ");
+		String deletingName = input.nextLine();
+		
+		if(contactsList.deleteContact(deletingName))
+			System.out.println("Contact has been deleted");
+		else
+			System.out.println("Contact was not found");
+	}
+	
+	private static int[] splitDateTime(String dateTimeStr) {
+        String[] parts = dateTimeStr.split(" ");
+        String[] dateParts = parts[0].split("/");
+        String[] timeParts = parts[1].split(":");
+
+        int year = Integer.parseInt(dateParts[2]);
+        int month = Integer.parseInt(dateParts[0]);
+        int day = Integer.parseInt(dateParts[1]);
+        int hour = Integer.parseInt(timeParts[0]);
+        int minutes = Integer.parseInt(timeParts[1]);
+
+        return new int[]{year, month, day, hour, minutes};
+    }
+	
+	public void scheduleEvent() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter event title: ");
+		String eventTitle = input.nextLine();
+		
+		System.out.println("Enter contact name: ");
+		String contactName = input.nextLine();
+		
+		Contact scheduledContact = contactsList.searchContactByName(contactName);
+		if (scheduledContact == null ) {
+			System.out.println("Contact was not found");
+			return;
+		}
+		
+		System.out.println("Enter event date and time (MM/DD/YYYY HH:MM)");
+		String eventDateAndTime = input.nextLine();
+		System.out.println("Enter event location: ");
+		String eventLocation = input.nextLine();
+		
+		int resultDateTime[] = splitDateTime(eventDateAndTime);
+		int year = resultDateTime[0];
+		int month = resultDateTime[1];
+		int dayOfMonth = resultDateTime[2];
+		int hour = resultDateTime[3];
+		int minute = resultDateTime[4];
+		
+		Event event = new Event(eventTitle, contactName, year, month, dayOfMonth, hour, minute, eventLocation);
+		scheduledContact.addEvent(event);
+		System.out.println("Event have been Scheduled");
+	}
+	
+	public void printEventDetails() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter search criteria: ");
+		System.out.println("1. Contact name: ");
+		System.out.println("2. Event title: ");
+
+		int choice = input.nextInt();
+		input.nextLine(); // to finish the line
 		
 		switch(choice) {
 		case 1:
 			System.out.println("Enter the contact's name: ");
-			String givenName = input.next();
-			System.out.println("Enter the contact's number: ");
-			String givenNumber = input.next();
-			System.out.println("Enter the contact's email address: ");
-			String givenEmail = input.next();
-			System.out.println("Enter the contact's address: ");
-			String givenAddress = input.next();
-			System.out.println("Enter the contact's birthday: ");
-			String givenBirthday = input.next();
-			System.out.println("Enter any notes for the contact");
-			String givenNotes = input.next();
-			
-			Contact c = new Contact(givenName, givenNumber, givenEmail, givenAddress, givenBirthday, givenNotes);			
-			l.addContact(c);
-			if(l.searchByAnyTerm(givenName).getName()==givenName)
-				System.out.println("contact succesfully added");
-		
-		case 2:
-			System.out.println("Enter search criteria: ");
-			System.out.println("1. Name");
-			System.out.println("2. Phone Number");
-			System.out.println("3. Email Address");
-			System.out.println("4. Address");
-			System.out.println("5. Birthday");
-			int Secondarychoice = input.nextInt();
-			
-			switch(Secondarychoice) {
-			case 1: System.out.println("Enter the Contact's name: "); String searchedName = input.next();
-			if(l.searchByAnyTerm(searchedName).getName()==searchedName) System.out.println("Contact is included");
-			
-			case 2: System.out.println("Enter Phone Number: "); String searchedNumber = input.next();
-			if(l.searchByAnyTerm(searchedNumber).getName()==searchedNumber) System.out.println("Contact is included");
-			
-			case 3: System.out.println("Enter Email Address: "); String searchedEmail = input.next();
-			if(l.searchByAnyTerm(searchedEmail).getName()==searchedEmail) System.out.println("Contact is included");
-			
-			case 4: System.out.println("Enter Address: "); String searchedAddress = input.next();
-			if(l.searchByAnyTerm(searchedAddress).getName()==searchedAddress) System.out.println("Contact is included");
-			
-			case 5: System.out.println("Enter Birthday:"); String searchedBirthday = input.next();
-			if(l.searchByAnyTerm(searchedBirthday).getName()==searchedBirthday) System.out.println("Contact is included");
+			String giveContactName = input.nextLine();
+			while(eventsList.current!=null) {
+				//if(eventsList.current             there is a problem here)
 			}
 			
-		case 3: 
-			System.out.println("Enter contact's name, Phone Number, Email Address, Address or Birthday: ");
-			String givenInfo = input.next();
-			l.deleteContact(l.searchByAnyTerm(givenInfo).getName());
+		case 2:
 			
-		case 4:
-			System.out.println("Enter even title:");
-			String givenEventTitle = input.next();
-			System.out.println("Enter contact's name: ");
-			String givenEventName = input.next();
-			System.out.println("Enter event date and time: ");
-			String givenEventDate = input.next();
-			System.out.println("Enter event location: ");
-			String givenEventLocation = input.next();
-			Event e = new Event(givenEventTitle, givenEventName, 2023, 10, 17, 11, 59, givenEventLocation);	
-			
-		case 5: //To be continued..
-		case 6:	//To be continued..
-		case 7:	//To be continued..
-		case 8:	//To be continued..
+			default:
+				break;
 		}
-		
 	}
-	while(true);
-		
-	}
+	
+	
+	
+	
+	
 }
